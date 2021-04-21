21b07b28c057cd37533ab7e04e14232ff42cdb67
hlmgrvs <hlmgrvs@gmail.com>
2021-04-21 22:04:58 +0200

    message interpolations

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,20 +3,34 @@ import {createI18n} from "vue-i18n";
 const messages = {
     lv: {
         routes: {
-            items: "Produkti",
-            about: "Par mums"
-        }
+            items: "Preces",
+            about: "Par mums",
+        },
+        cart: {
+            cart: "Grozs",
+            subtotal: "Kopā",
+            clearButton: "Izņemt preci"
+        },
+        popularItem: "Vispopulārākā prece šajā veikalā ir {itm}, vismaz uz kādu laiku."
     },
     ru: {
         routes: {
             items: "Продукты",
             about: "О нас"
-        }
+        },
+        cart: {
+            cart: "Корзина",
+            subtotal: "Итого",
+            clearButton: "Удалить товар"
+        },
+        popularItem: "Самый популярный товар в нашем магазине - {itm}, но это не на долго."
     },
 }
 
 export default createI18n({
     locale: "lv",
     legacy: false,
+    fallbackLocale: "lv",
+    globalInjection: true,
     messages
 });
